4bf38c16fa147d36fdf922663916090641f7308b
gneginskiy <gneginskiy@mail.ru>
2023-07-21 14:35:16 +0400

    added spring projects for query and command

--- command_api/src/main/java/com/techbank/account/cmd/service/AccountEventStoreService.java
+++ command_api/src/main/java/com/techbank/account/cmd/service/AccountEventStoreService.java
@@ -1,5 +1,6 @@
 package com.techbank.account.cmd.service;
 
+import com.techbank.account.base.EventProducer;
 import com.techbank.account.cmd.aggregates.AccountAggregate;
 import com.techbank.account.cmd.repository.EventStoreRepository;
 import com.techbank.account.cmd.exceptions.ConcurrencyException;
@@ -18,6 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AccountEventStoreService implements EventStoreService {
     private final EventStoreRepository eventStoreRepository;
+    private final AccountEventProducer accountEventProducer;
 
     @Override
     public void saveEvents(String aggregateId, Collection<BaseEventDto> events, int expectedVersion) {
@@ -25,23 +27,23 @@ public class AccountEventStoreService implements EventStoreService {
         if (expectedVersion != -1 && allEvents.get(allEvents.size() - 1).getVersion() != expectedVersion) {
             throw new ConcurrencyException();
         }
-
         var version = expectedVersion;
         for (var e : events) {
             e.setVersion(++version);
-            var eventModel = EventEntity
-                    .builder()
-                    .timestamp(Instant.now().toEpochMilli())
-                    .aggregateId(aggregateId)
-                    .aggregateType(AccountAggregate.class.getTypeName())
-                    .eventData(e)
-                    .version(version)
-                    .build();
-            var persistedEvent = eventStoreRepository.save(eventModel);
-            if (persistedEvent != null) {
-                //todo: produce event to KAFKA
-            }
+            eventStoreRepository.save(toEventEntity(aggregateId, version, e));
+            accountEventProducer.produce(e.getClass().getSimpleName(), e);
         }
+    }
+
+    private static EventEntity toEventEntity(String aggregateId, int version, BaseEventDto e) {
+        return EventEntity
+                .builder()
+                .timestamp(Instant.now().toEpochMilli())
+                .aggregateId(aggregateId)
+                .aggregateType(AccountAggregate.class.getTypeName())
+                .eventData(e)
+                .version(version)
+                .build();
     }
 
     @Override
