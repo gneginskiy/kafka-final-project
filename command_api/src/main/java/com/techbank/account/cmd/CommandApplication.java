01b5f03e9550bcc51aceb221ad0d81c21ed72c15
gneginskiy <gneginskiy@mail.ru>
2023-07-21 18:13:00 +0400

    added spring projects for query and command

--- command_api/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ command_api/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -1,6 +1,5 @@
 package com.techbank.account.cmd;
 
-import com.techbank.account.base.service.CommandDispatcherService;
 import com.techbank.account.cmd.commands.CloseAccountCommand;
 import com.techbank.account.cmd.commands.DepositFundsCommand;
 import com.techbank.account.cmd.commands.OpenAccountCommand;
@@ -14,21 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class CommandApplication {
-    private final CommandDispatcherService cmdDispatcher;
-    private final AccountCommandHandlerService handler;
 
     public static void main(String[] args) {
         SpringApplication.run(CommandApplication.class, args);
-    }
-
-
-	//todo: extract to configuration.
-    @PostConstruct
-    public void registerCommandHandlers() {
-		cmdDispatcher.registerCommand(OpenAccountCommand.class,   handler::handle);
-		cmdDispatcher.registerCommand(CloseAccountCommand.class,  handler::handle);
-		cmdDispatcher.registerCommand(DepositFundsCommand.class,  handler::handle);
-		cmdDispatcher.registerCommand(WithdrawFundsCommand.class, handler::handle);
     }
 
 }
