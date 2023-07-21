fab797df7319a8542fbc025fb3a9959995d9b0ac
gneginskiy <gneginskiy@mail.ru>
2023-07-21 19:33:44 +0400

    added spring projects for query and command

--- command_api/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ command_api/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -1,17 +1,9 @@
 package com.techbank.account.cmd;
 
-import com.techbank.account.cmd.commands.CloseAccountCommand;
-import com.techbank.account.cmd.commands.DepositFundsCommand;
-import com.techbank.account.cmd.commands.OpenAccountCommand;
-import com.techbank.account.cmd.commands.WithdrawFundsCommand;
-import com.techbank.account.cmd.service.AccountCommandHandlerService;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class CommandApplication {
 
     public static void main(String[] args) {
