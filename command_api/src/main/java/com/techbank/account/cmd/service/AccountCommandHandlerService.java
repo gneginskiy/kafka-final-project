657d017e39877be2ad1c7f397b6677684081a399
gneginskiy <gneginskiy@mail.ru>
2023-07-26 00:20:28 +0400

    added spring projects for query and command

--- command_api/src/main/java/com/techbank/account/cmd/service/AccountCommandHandlerService.java
+++ command_api/src/main/java/com/techbank/account/cmd/service/AccountCommandHandlerService.java
@@ -17,56 +17,36 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AccountCommandHandlerService {
-    private final EventSourcingService<AccountAggregate> eventSourcingService;
     private final AccountCommandValidator validator;
     private final AccountCommandToEventMapper eventMapper;
     private final AccountAggregateService aggregateService;
     private final AccountEventSender eventSender;
 
     public void handle(OpenAccountCommand cmd) {
-        //todo: complete:
         validator.validate(cmd);
         AccountOpenedEvent event = eventMapper.getEvent(cmd);
         aggregateService.apply(event);
         eventSender.send(event);
-
-//        eventSourcingService.saveAggregate(new AccountAggregate(cmd));
     }
 
     public void handle(CloseAccountCommand cmd) {
-        //todo: complete
         validator.validate(cmd);
         AccountClosedEvent event = eventMapper.getEvent(cmd);
         aggregateService.apply(event);
         eventSender.send(event);
-
-        var aggregate = eventSourcingService.getAggregateById(cmd.getId());
-        aggregate.close();
-        eventSourcingService.saveAggregate(aggregate);
     }
 
     public void handle(WithdrawFundsCommand cmd) {
-        //todo: complete
         validator.validate(cmd);
         AccountFundsWithdrawnEvent event = eventMapper.getEvent(cmd);
         aggregateService.apply(event);
         eventSender.send(event);
-
-        validator.validate(cmd);
-        var aggregate = eventSourcingService.getAggregateById(cmd.getId());
-        aggregate.withdrawFunds(cmd.getAmount());
-        eventSourcingService.saveAggregate(aggregate);
     }
 
     public void handle(DepositFundsCommand cmd) {
-        //todo: complete
         validator.validate(cmd);
         AccountFundsDepositedEvent event = eventMapper.getEvent(cmd);
         aggregateService.apply(event);
         eventSender.send(event);
-
-        //        var aggregate = eventSourcingService.getAggregateById(cmd.getId());
-//        aggregate.depositFunds(cmd.getAmount());
-//        eventSourcingService.saveAggregate(aggregate);
     }
 }
