45a071413f98f9af0a663b7ddca45c2b17deb980
gneginskiy <gneginskiy@mail.ru>
2023-07-26 03:42:15 +0400

    added spring projects for query and command

--- command_api/src/main/java/com/techbank/account/cmd/controller/AccountCommandController.java
+++ command_api/src/main/java/com/techbank/account/cmd/controller/AccountCommandController.java
@@ -4,14 +4,13 @@ import com.techbank.account.cmd.commands.CloseAccountCommand;
 import com.techbank.account.cmd.commands.DepositFundsCommand;
 import com.techbank.account.cmd.commands.OpenAccountCommand;
 import com.techbank.account.cmd.commands.WithdrawFundsCommand;
+import com.techbank.account.cmd.exceptions.ApiError;
 import com.techbank.account.cmd.service.AccountCommandHandlerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -24,32 +23,32 @@ public class AccountCommandController {
 
     private final AccountCommandHandlerService accountService;
 
-    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody OpenAccountCommand cmd) {
-        accountService.handle(cmd);
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> create(@RequestBody OpenAccountCommand cmd) {
+        return ResponseEntity.ok(accountService.handle(cmd));
     }
 
-    @PostMapping(path = "/{id}/close", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(path = "/{id}/close", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public void create(@RequestBody CloseAccountCommand cmd) {
         accountService.handle(cmd);
     }
 
 
-    @PostMapping(path = "/{id}/withdraw", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(path = "/{id}/withdraw", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public void create(@RequestBody WithdrawFundsCommand cmd) {
         accountService.handle(cmd);
     }
 
-    @PutMapping(path = "/{id}/deposit", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping(path = "/{id}/deposit", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public void create(@RequestBody DepositFundsCommand cmd) {
         accountService.handle(cmd);
     }
 
-    @ExceptionHandler(IllegalStateException.class)
+    @ExceptionHandler(ApiError.class)
     protected ResponseEntity<?> handle(Exception e) {
         log.error(e.getMessage(), e);
         return ResponseEntity.badRequest().body(e);
