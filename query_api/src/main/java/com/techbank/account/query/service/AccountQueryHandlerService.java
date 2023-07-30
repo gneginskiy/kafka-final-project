68252c8d962be89a61cc1ca37ce25e6d72d21535
gneginskiy <gneginskiy@mail.ru>
2023-07-30 19:32:31 +0400

    concurrent producers

--- query_api/src/main/java/com/techbank/account/query/service/AccountQueryHandlerService.java
+++ query_api/src/main/java/com/techbank/account/query/service/AccountQueryHandlerService.java
@@ -3,9 +3,12 @@ package com.techbank.account.query.service;
 import com.techbank.account.query.controller.GetAllRqDetails;
 import com.techbank.account.query.dto.AccountDto;
 import com.techbank.account.query.dto.PaginatedList;
+import com.techbank.account.query.entity.AccountEntity;
 import com.techbank.account.query.repository.AccountRepository;
 import com.techbank.account.query.validation.AccountQueryValidator;
+import converter.UnifiedMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,6 +18,7 @@ import java.util.UUID;
 public class AccountQueryHandlerService {
     private final AccountQueryValidator validator;
     private final AccountRepository accountRepository;
+    private final UnifiedMapper mapper;
 
     public AccountDto get(UUID id) {
         validator.validateGet(id);
@@ -22,7 +26,8 @@ public class AccountQueryHandlerService {
         return null;
     }
 
-    public PaginatedList<AccountDto> getAll(GetAllRqDetails of) {
-        return null;
+    public PaginatedList<AccountDto> getAll(GetAllRqDetails rq) {
+        var page = accountRepository.findAll(rq.predicate(), rq.pageable());
+        return PaginatedList.of(page, mapper::toDto);
     }
 }
