f75e0cb38210c7554d4ec1836259f7318102edde
gneginskiy <gneginskiy@mail.ru>
2023-07-31 00:47:50 +0400

    concurrent producers

--- query_api/src/main/java/com/techbank/account/query/service/AccountQueryHandlerService.java
+++ query_api/src/main/java/com/techbank/account/query/service/AccountQueryHandlerService.java
@@ -16,7 +16,6 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class AccountQueryHandlerService {
-    private final AccountQueryValidator validator;
     private final AccountRepository accountRepository;
     private final UnifiedMapper mapper;
 
